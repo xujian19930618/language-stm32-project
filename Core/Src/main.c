9026edfd0c5214e2dcea28612496eee9c6db0c94
徐剑 <xujian19930618@163.com>
2025-03-15 02:00:19 +0800

    提交

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,6 +94,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
