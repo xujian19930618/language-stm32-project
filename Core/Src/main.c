46d6ad018b68a509f0c3e5e9e98a403fd2571b5f
xujian <xujian19930618@163.com>
2025-04-03 09:24:49 +0800

    提交

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,11 +128,11 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  // MX_USART1_UART_Init();
-  // MX_SPI3_Init();
-  // MX_RTC_Init();
-  // MX_SDIO_SD_Init();
-  // MX_USB_DEVICE_Init();
+  MX_USART1_UART_Init();
+  MX_SPI3_Init();
+  MX_RTC_Init();
+  MX_SDIO_SD_Init();
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
