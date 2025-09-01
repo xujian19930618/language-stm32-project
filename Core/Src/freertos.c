e6d138d020d26d7f2436b3fe54e1e1ed08af1515
xujian <xujian19930618@163.com>
2025-09-01 13:53:56 +0800

    提交

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -29,7 +29,6 @@
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
-typedef StaticEventGroup_t osStaticEventGroupDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
@@ -54,14 +53,6 @@ const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
-};
-/* Definitions for event */
-osEventFlagsId_t eventHandle;
-osStaticEventGroupDef_t eventControlBlock;
-const osEventFlagsAttr_t event_attributes = {
-  .name = "event",
-  .cb_mem = &eventControlBlock,
-  .cb_size = sizeof(eventControlBlock),
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -107,10 +98,6 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
-
-  /* Create the event(s) */
-  /* creation of event */
-  eventHandle = osEventFlagsNew(&event_attributes);
 
   /* USER CODE BEGIN RTOS_EVENTS */
   /* add events, ... */
