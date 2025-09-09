34d1b74355e14a2d9f2ac12c0bbbbc9f17077a80
xujian <xujian19930618@163.com>
2025-09-09 20:20:59 +0800

    提交

--- User/LCD/Inc/lcd_test.h
+++ User/LCD/Inc/lcd_test.h
@@ -5,9 +5,8 @@
 #ifndef LANGUAGE_STM32_PROJECT_LCD_TEST_H
 #define LANGUAGE_STM32_PROJECT_LCD_TEST_H
 
-
-
 #include "lcd_spi_154.h"
+
 void LCD_Test_Text();
 void LCD_Test_Variable ();
 void LCD_Test_Color();
