87bf5f9f7885a219f05c772cc438b4f775badbec
徐剑 <xujian19930618@outlook.com>
2026-05-08 10:06:37 +0800

    提交

--- User/LCD/Inc/lcd_test.h
+++ User/LCD/Inc/lcd_test.h
@@ -7,12 +7,12 @@
 
 #include "lcd_spi_154.h"
 
-void LCD_Test_Text();
-void LCD_Test_Variable ();
-void LCD_Test_Color();
-void LCD_Test_Grahic();
-void LCD_Test_Image();
-void  LCD_Test_Direction();
-void LCD_Test_Clear();
+void LCD_Test_Text(void);
+void LCD_Test_Variable(void);
+void LCD_Test_Color(void);
+void LCD_Test_Grahic(void);
+void LCD_Test_Image(void);
+void LCD_Test_Direction(void);
+void LCD_Test_Clear(void);
 
 #endif //LANGUAGE_STM32_PROJECT_LCD_TEST_H
